47a6d1b76bb7a5d16783796d4e0e19855cec6557
Bárbara Menezes <barbara.menezes@live.com>
2024-11-18 01:18:52 -0300

    Fix unused imports

--- automatizado/src/test/java/automatizado/test/ReportIssueTest.java
+++ automatizado/src/test/java/automatizado/test/ReportIssueTest.java
@@ -2,16 +2,12 @@ package automatizado.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import automatizado.page.LoginPO;
-import automatizado.page.MyViewPO;
 import automatizado.page.ReportIssuePO;
 import io.github.cdimascio.dotenv.Dotenv;
 
